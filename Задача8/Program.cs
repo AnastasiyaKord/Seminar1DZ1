912851caf9a380c7ab01951cf531da3e41962dc4
Anastasiya <vipstop2303@mail.ru>
2023-07-26 12:57:19 +0300

    задача8

---
+++ Задача8/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 8: Напишите программу, которая на вход 
+// принимает число (N), а на выходе показывает 
+// все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите четное число: ");
+string answerA = (Console.ReadLine()) ?? "";
+int N = int.Parse(answerA);
+int i = 2;
+
+if (0 == N % 2)
+{
+
+    while (i <= N)
+    {
+
+        Console.Write(i + ", ");
+        i = i + 2;
+    }
+}
+else 
+Console.WriteLine("Это нечетное число");
+
+
